50198ffde81e238f9dbd296822389c929bde3169
henriqk0 <171154572+henriqk0@users.noreply.github.com>
2025-06-23 20:02:08 -0300

    feat: comecando a construcao do app

--- src/main/java/com/app/Application.java
+++ src/main/java/com/app/Application.java
@@ -3,9 +3,7 @@ package com.app;
 import com.app.view.Menu;
 
 public class Application {
-    public static void main(String[] args) {
-        Menu.show();
-        
-        
-    }
+  public static void main(String[] args) {
+    Menu.run();
+  }
 }
--- src/main/java/com/app/view/Menu.java
+++ src/main/java/com/app/view/Menu.java
@@ -1,32 +1,60 @@
 package com.app.view;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Menu {
 
-    public static void show() {
-        System.out.println("╔═══════════════════════════════╗                                        ");
-        System.out.println("║           BEM VINDO!          ║          «░░▒▒╗                       ");
-        System.out.println("╠═══════════════════════════════╣               ▐                        ");
-        System.out.println("║      Selecione uma opção:     ║             ░▓▐▓▒░                      ");
-        System.out.println("║ ───────────────────────────── ║            ░▒▓▐▓▓▓▒▒░                   ");
-        System.out.println("║   1) Criar um mapa            ║           ░▒▓▓▐▓▓▓▓▓▒▒▒░                       ");
-        System.out.println("║   2) Adicionar um destino     ║          ░▒▒▓▓▐▓▓▓▓▓▓▓▒▒▒░░                       ");
-        System.out.println("║   3) Visualizar mapa          ║    ▀█▄▄▄▄▄    ▐       ▄▄▄▄▄▄▄▄▄           ");
-        System.out.println("║   4) Rota global mais segura  ║     ▀████████████████████████         "); // arvore geradora minima
-        System.out.println("║   5) Melhor caminho A->B      ║       ▀██████████████████▀▀           ");
-        System.out.println("║   0) Sair                     ║      «▓▓▓▀▀██████████▀▀▀                ");
-        System.out.println("╚═══════════════════════════════╝      «╜      ▀▀▀▀                      ");
-    }
+  public static void show() {
+    System.out.println("╔═══════════════════════════════╗                                        ");
+    System.out.println("║           BEM VINDO!          ║          «░░▒▒╗                       ");
+    System.out.println("╠═══════════════════════════════╣               ▐                        ");
+    System.out.println("║      Selecione uma opção:     ║             ░▓▐▓▒░                      ");
+    System.out.println("║ ───────────────────────────── ║            ░▒▓▐▓▓▓▒▒░                   ");
+    System.out.println("║   1) Criar um mapa            ║           ░▒▓▓▐▓▓▓▓▓▒▒▒░                       ");
+    System.out.println("║   2) Adicionar um destino     ║          ░▒▒▓▓▐▓▓▓▓▓▓▓▒▒▒░░                       ");
+    System.out.println("║   3) Visualizar mapa          ║    ▀█▄▄▄▄▄    ▐       ▄▄▄▄▄▄▄▄▄           ");
+    System.out.println("║   4) Rota global mais segura  ║     ▀████████████████████████         "); // arvore geradora minima
+    System.out.println("║   5) Melhor caminho A->B      ║       ▀██████████████████▀▀           ");
+    System.out.println("║   0) Sair                     ║      «▓▓▓▀▀██████████▀▀▀                ");
+    System.out.println("╚═══════════════════════════════╝      «╜      ▀▀▀▀                      ");
+  }
 
-    
-    public static void clearConsole() {
-        try {
-            if (System.getProperty("os.name").contains("Windows")) {
-                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
-            }
-            else { System.out.print("\033\143"); }
-        } catch (IOException | InterruptedException ex) {}
-    }
+  public static void run() {
+    Scanner scanner = new Scanner(System.in);
 
+    Menu.show();        
+    int opt = scanner.nextInt();
+
+    while (opt != 0) {
+      if (opt < 0 && opt < 6) {
+
+        Menu.clearConsole();
+        if ( opt == 2 ) { 
+          System.out.println("Digite o destino a ser adicionado:");
+        }
+        else if (opt == 1) { 
+
+        }
+        else if (opt == 3) { }
+        else if (opt == 4) { }
+        else  { }
+      }
+      else { System.out.println("Opcao invalida!"); }
+
+      Menu.show();
+      opt = scanner.nextInt();
+    }
+    scanner.close();
+  }
+
+  
+  public static void clearConsole() {
+    try {
+      if (System.getProperty("os.name").contains("Windows")) {
+        new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+      }
+      else { System.out.print("\033\143"); }
+    } catch (IOException | InterruptedException ex) {}
+  }
 }
